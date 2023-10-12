8941bb1214648c87173404b16b21ea7b97b1681f
xabi1000 <babelwado@gmail.com>
2023-10-12 22:26:50 +0100

    add netlify config adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-netlify';
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter({
+      edge: false,
+      split: false,
+    }),
+  },
+};
