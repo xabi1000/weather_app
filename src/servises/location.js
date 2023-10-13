425e282cd3be00c7fa2945773fa3d6289525852f
xabi1000 <babelwado@gmail.com>
2023-10-13 14:58:08 +0100

    refactoring the user's position fetching function

--- src/servises/location.js
+++ src/servises/location.js
@@ -1,8 +1,23 @@
 export async function GET_IP() {
-  const url = 'http://ip-api.com/json/?fields=61439';
-  const response = await fetch(url);
-  const data = await response.json();
-  const { country, lat, lon } = data;
+  let endpoint =
+    'http://ip-api.com/json/?fields=status,message,country,lat,lon';
 
-  return country || `${lat}, ${lon}`;
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    if (data.status !== 'success') {
+      console.log('Failed consultation: ' + data.message);
+      return null;
+    }
+
+    if (data.country) {
+      return data.country;
+    } else {
+      return `${data.lat}, ${data.lon}`;
+    }
+  } catch (error) {
+    console.error('Consultation error:', error);
+    return null;
+  }
 }
